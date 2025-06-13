345051d20e13a5090f168c3372c4394f79ef73a8
Rishwanth1121 <rishwanth1121@gmail.com>
2025-06-13 17:18:40 +0530

    New files added

--- social_platform/chat/views.py
+++ social_platform/chat/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from .models import Message
 
 def room(request, room_name):
-    username = request.GET.get("username", "Anonymous")
-    messages = Message.objects.filter(room_name=room_name).order_by('timestamp')  # Adjust field names
+    username = request.user.username if request.user.is_authenticated else "Anonymous"
+    messages = Message.objects.filter(room_name=room_name).order_by('timestamp')
     return render(request, "chat/room.html", {
         "room_name": room_name,
         "username": username,
         "messages": messages,
     })
+
